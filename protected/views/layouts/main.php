5fc6aca5c575a68db6c67d61c3e9ffb8976b82f5
imbugs <imbugs@126.com>
2013-01-23 11:29:04 +0800

    google analytics

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -14,8 +14,15 @@
 	<![endif]-->
 	<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/dist/app/tp-all.js"></script>
 	<script type="text/javascript">
-	<!--
-	//-->
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-37882850-1']);
+	_gaq.push(['_trackPageview']);
+	
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
 	</script>
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
